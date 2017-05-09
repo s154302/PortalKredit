20ffd8ce59c5856df6cf61c3108cac0dc7fcf727
Simon Heimdal <simonheimdal@gmail.com>
2017-05-09 14:27:10 +0200

    DeleteAdmin startet på
    halp pls

---
+++ PortalKredit/WebContent/WEB-INF/classes/DeleteAdminServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import classes.Controller;
+
+@WebServlet("/admin/DeleteAdminServlet")
+public class DeleteAdminServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+
+
+}
