4269ea204bf35ffcd0e0489eca50975176d549e0
tk7000 <s154219@student.dtu.dk>
2017-06-07 17:11:34 +0200

    Made TRANSACTIONSOLD
    Made changes to the Setup.sql file, it should be correct now
    Made a method for backuping the transactions

--- PortalKredit/src/classes/Controller.java
+++ PortalKredit/src/classes/Controller.java
@@ -339,6 +339,32 @@ public final class Controller {
 		}
 		return transactionList;
 	}
+	
+	public static ArrayList<Transaction> getOldTransactions(String accountNumber, int regNo, DataSource ds1) {
+
+		ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
+		Connection con;
+		try {
+			con = ds1.getConnection(Secret.userID, Secret.password);
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT * FROM \"DTUGRP16\".\"TRANSACTIONOLD\" WHERE \"ACCOUNTNUMBER\" = ? AND \"REGNO\" = ?");
+
+			ps.setString(1, accountNumber);
+			ps.setInt(2, regNo);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				transactionList.add(new Transaction(rs.getString("TRANSACTIONID"), rs.getString("ACCOUNTNUMBER"),
+						rs.getInt("REGNO"), rs.getString("RECIEVEACCOUNT"), rs.getInt("RECIEVEREGNO"),
+						rs.getDate("DATEOFTRANSACTION"), rs.getDouble("AMOUNT"), rs.getString("CURRENCY"), rs.getDouble("BALANCE"), rs.getString("NOTE")));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return transactionList;
+	}
 
 	// Fills a single Admin object with data and returns it
 	public static Admin getAdminInfo(String userID, DataSource ds1) {
@@ -969,6 +995,29 @@ public final class Controller {
 							+ "\"DTUGRP16\".\"ACCOUNT\".\"BALANCE\" + \"DTUGRP16\".\"ACCOUNT\".\"INTEREST\""
 							+ ", \"DTUGRP16\".\"ACCOUNT\".\"INTEREST\" = 0");
 			int rs =ps.executeUpdate();
+			status = true;
+		}catch(Exception e){
+			e.printStackTrace();
+			status = false;
+		}
+		
+		
+		return status;
+	}
+	
+	public static boolean backupTransactions(DataSource ds1){
+		Boolean status = false;
+		
+		try{
+			Connection con = ds1.getConnection(Secret.userID, Secret.password);
+			
+			PreparedStatement movePs = con
+					.prepareStatement("INSERT INTO \"DTUGRP16\".\"TRANSACTIONOLD\" SELECT * FROM \"DTUGRP16\".\"TRANSACTION\"");
+			int rs =movePs.executeUpdate();
+			
+			//TODO - der burde nok lige blive checket om de er blevet flyttet først
+			PreparedStatement deletePs = con
+					.prepareStatement("DELETE FROM \"DTUGRP16\".\"TRANSACTION\"");
 			status = true;
 		}catch(Exception e){
 			e.printStackTrace();
