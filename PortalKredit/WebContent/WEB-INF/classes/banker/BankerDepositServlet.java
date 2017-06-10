c6a1a7d0177415f18467fb31c9b0e155b8bb4cf0
tk7000 <s154219@student.dtu.dk>
2017-06-10 12:46:26 +0200

    One can now deposit money

--- PortalKredit/WebContent/WEB-INF/classes/banker/BankerDepositServlet.java
+++ PortalKredit/WebContent/WEB-INF/classes/banker/BankerDepositServlet.java
@@ -12,21 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
+import classes.Banker;
 import classes.Controller;
 
 @WebServlet("/banker/Deposit")
 public class BankerDepositServlet extends HttpServlet {
 	//Copied from clientPayment
 	private static final long serialVersionUID = 1L;
-	String sendAcc, reciAcc, stramount, strsendReg, strreciReg, currency, strmessage, strreciMessage,
-	fullAcc, password;
-	
-	
-	public BankerDepositServlet(){
-		super();
-	}
-
-	
+	String accountNumber, amount, currency, userId, password;
+		
 	@Resource(name = "jdbc/exampleDS")
 	private DataSource ds1;
 	
@@ -48,66 +42,45 @@ public class BankerDepositServlet extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.setContentType("text/html");
-//		HttpSession session = request.getSession();
-//		if(Controller.checkAuth(Controller.Type.client, session)){
-//			payment(request, response, session);
-//		}
-//		else{
-//			request.getSession().invalidate();
-//			response.sendRedirect("../index");
-//		}
-//		
+		response.setContentType("text/html");
+		HttpSession session = request.getSession();
+		Connection con = Controller.getConnection(ds1);
+		if(Controller.checkAuth(Controller.Type.banker, session)){
+			Banker banker = (Banker)session.getAttribute("user");
+			accountNumber = request.getParameter("reciAcc");
+			amount = request.getParameter("amount");
+			currency = request.getParameter("currency");
+			userId = (String) session.getAttribute("userID");
+			password = request.getParameter("password");
+			
+			Boolean correctPw = Controller.authenticate(userId, password, con, session);
+			
+			if(correctPw){
+				if(Controller.deposit(accountNumber, banker.getRegNo(), con, Double.parseDouble(amount), currency)){
+					request.setAttribute("status", "Sussces");
+				}else{
+					request.setAttribute("status", "Error somthing went wrong");
+					request = keepInputs(request);
+				}
+			}else{
+				request.setAttribute("status", "Wrong password");
+				request = keepInputs(request);
+			}
+		}
+		else{
+			request.getSession().invalidate();
+			response.sendRedirect("../index");
+		}
+		
+		Controller.cleanUpConnection(con);
+		request.getRequestDispatcher("Deposit.jsp").forward(request, response);
+		
 	}
-//	
-//	//Used to keep the written input in the input fields
-//	private HttpServletRequest keepInputs(HttpServletRequest request){	
-//		request.setAttribute("amount", stramount);
-//		request.setAttribute("currency", currency);
-//		request.setAttribute("message", strmessage);
-//		request.setAttribute("reciMessage", strreciMessage);
-//		request.setAttribute("reciReg", strreciReg);
-//		request.setAttribute("reciAcc", reciAcc);
-//		return request;
-//	}
-//	private void payment(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException{
-//		fullAcc = request.getParameter("senderAcc");
-//		String[] fullAccSplit = fullAcc.split("[.]");
-//		strsendReg = fullAccSplit[0];
-//		sendAcc = fullAccSplit[1];
-//		reciAcc = request.getParameter("reciAcc");
-//		strreciReg = request.getParameter("reciReg");
-//		stramount = request.getParameter("amount");
-//		currency = request.getParameter("currency");
-//		strmessage = request.getParameter("message");
-//		strreciMessage = request.getParameter("reciMessage");
-//		password = request.getParameter("password");
-//		String sendReg = strsendReg;
-//		String reciReg = strreciReg;
-//		double amount = Double.parseDouble(stramount);
-//		
-//		Connection con = Controller.getConnection(ds1);
-//		String userID = (String) session.getAttribute("userID");
-//		Boolean correctPw = Controller.authenticate(userID, password, con, session);
-//
-//		if(correctPw){
-//			Boolean status = Controller.transaction(sendAcc, reciAcc, amount, sendReg, reciReg,
-//					currency, strmessage, strreciMessage, con);
-//			
-//			if(status){
-//				request.setAttribute("status", "Payment complete");
-//			}else{
-//				request.setAttribute("status", "Payment incomplete - somthing went wrong");
-//				request = keepInputs(request); 
-//				
-//			}
-//		}else{
-//			request.setAttribute("status", "Payment incomplete - Wrong password");
-//			request = keepInputs(request); 
-//		}
-//		
-//		Controller.cleanUpConnection(con);
-//		request.getRequestDispatcher("payments.jsp").forward(request, response);
-//		
-//	}
+	
+	private HttpServletRequest keepInputs(HttpServletRequest request){	
+		request.setAttribute("amount", amount);
+		request.setAttribute("currency", currency);
+		request.setAttribute("reciAcc", accountNumber);
+		return request;
+	}
 }
